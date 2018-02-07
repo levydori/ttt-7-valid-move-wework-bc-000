d134364b370bfb1654b10518d389563621368144
Dori Levy <dori.levy@wework.com>
2018-02-07 02:47:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,6 +10,5 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, pos)
-  empty_space = ["", " ", nil]
-  return !empty_space.include?(board[pos])
+  return true
 end
