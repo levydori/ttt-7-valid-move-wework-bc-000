e266ca16b79c142c91b0bfaadd2aba267a7a1024
Dori Levy <dori.levy@wework.com>
2018-02-07 02:45:03 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
 # code your #valid_move? method here
+
 def valid_move?(array, pos)
   if pos.between?(0, 8) && position_taken?(array, pos)
     return false
@@ -8,5 +9,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, pos)
-  return !["", " ", nil].include?(array[pos])
+  empty_space = ["", " ", nil]
+  return ! empty_space.include?(array[pos])
 end
