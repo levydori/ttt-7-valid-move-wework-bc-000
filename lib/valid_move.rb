5de73f6d69cef40c81581ceda6f5b7f62a9adb05
Dori Levy <dori.levy@wework.com>
2018-02-07 02:37:53 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?(array, pos)
+  if pos < 0 or pos > 8
+    return false
+  else 
+  
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
