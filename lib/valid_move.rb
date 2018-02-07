eda3d9c5ac15c5b4bca7a2ee5286f20ec0a2a403
Dori Levy <dori.levy@wework.com>
2018-02-07 02:49:31 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
 # code your #valid_move? method here
 
+def valid_move?(board, pos)
+  if !pos.between?(0, 8) || position_taken?(board, pos)
+    return false
+  else
+    return true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, pos)
+  empty_space = ["", " ", nil]
+  return !empty_space.include?(board[pos])
+end
