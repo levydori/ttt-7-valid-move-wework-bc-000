eae9b96dacf6949306307672feae80e4fd74cf8c
Dori Levy <dori.levy@wework.com>
2018-02-07 02:47:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,15 @@
 # code your #valid_move? method here
 
-def valid_move?(array, pos)
-  if pos.between?(0, 8) && position_taken?(array, pos)
+def valid_move?(board, pos)
+  if pos.between?(0, 8) && position_taken?(board, pos)
     return false
   else
     return true
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(array, pos)
+
+def position_taken?(board, pos)
   empty_space = ["", " ", nil]
-  return ! empty_space.include?(array[pos])
+  return !empty_space.include?(board[pos])
 end
